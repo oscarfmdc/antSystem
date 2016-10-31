1449b108eb9dab58b362f8c500ca38a4d7ed6098
Borja Illescas <mrbim18@gmail.com>
2016-10-31 18:42:31 +0100

    Mejor resultado multiobjetivo

--- singleObjective/execute.sh
+++ singleObjective/execute.sh
@@ -13,13 +13,20 @@
 # l: Busqueda local -  0: no local search   1: 2-opt   2: 2.5-opt   3: 3-opt
 # r : intentos
 
+rm ./results/*
 cd ./results
 
 # Problema a280
 ../src/acotsp -i ../problems/a280.tsp -o 2579 -t 60 -m 280 -b 5
 
 # Problema att48
-../src/acotsp -i ../problems/att48.tsp -o 10628 -t 60 -m 48 -b 5
+#../src/acotsp -i ../problems/att48.tsp -o 10628 -t 60 -m 48 -b 5
 
 # Problema berlin52
-../src/acotsp -i ../problems/berlin52.tsp -o 7542 -t 60 -m 52 -b 5
+#../src/acotsp -i ../problems/berlin52.tsp -o 7542 -t 60 -m 52 -b 5
+
+# Problema u1432
+../src/acotsp -i ../problems/u1432.tsp -o 152970 -t 60 -m 1022 -b 5
+
+# Problema fnl4461
+#../src/acotsp -i ../problems/fnl4461.tsp -o 182566 -t 60 -m 1022 -b 5
