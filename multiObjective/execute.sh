cb0717a92ec275314531f404fcaad0b23c639bd6
Borja Illescas <mrbim18@gmail.com>
2016-11-01 12:59:35 +0100

    Multiobjetivo con euclidAB100

--- multiObjective/execute.sh
+++ multiObjective/execute.sh
@@ -2,5 +2,8 @@
 # Resuelve el problema kroAB100 multiobjetivo en 100 segundos
  cd ./moaco/trunk
  
- ~/bin/moaco_btsp --input ../../btsp/kroAB100.tsp --time 100 --BicriterionAnt > ../../resultsBicriterion
- ~/bin/moaco_btsp --input ../../btsp/kroAB100.tsp --time 100 --PACO > ../../resultsPACO
+#~/bin/moaco_btsp --input ../../btsp/kroAB100.tsp --time 100 --BicriterionAnt > ../../resultsBicriterion
+#~/bin/moaco_btsp --input ../../btsp/kroAB100.tsp --time 100 --PACO > ../../resultsPACO
+
+~/bin/moaco_btsp --input ../../btsp/euclidAB100.tsp --time 100 --BicriterionAnt > ../../resultsBicriterion
+~/bin/moaco_btsp --input ../../btsp/euclidAB100.tsp --time 100 --PACO > ../../resultsPACO
